32343675704edf139acb7491858f58a5c4a69596
Tamas <tamas.veingartner@r3.com>
2021-07-04 21:14:36 +0100

    code cleanup

--- ConfirmedAPI/ConfirmedAPI/Models/Stock.cs
+++ ConfirmedAPI/ConfirmedAPI/Models/Stock.cs
@@ -1,21 +1,11 @@
-﻿using DataAnnotationsExtensions;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConfirmedAPI.Models
 {
     public class Stock
     {
-        //[Timestamp]
-        //public byte[] RowVersion { get; set; }
-        //[ConcurrencyCheck]
-        //[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        //[NotMapped]
-        //public string xmin { get; set; }
         [Key]
         public int ID { get; set; }
         public uint xmin { get; set; }
